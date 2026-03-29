236b55f1a7e99fceb51422a7e8a5823f46836b91
kunndnn <44980639+kunndnn@users.noreply.github.com>
2026-03-29 23:22:41 +0530

    switch case

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,10 @@
-import { Text, View, Button, Alert } from "react-native";
+import { useState } from "react";
+import { Text, View, Button, Alert, Switch } from "react-native";
 
 export default function Index() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
   const pressMe = () => {
     Alert.alert("Button clicked.");
   };
@@ -8,6 +12,7 @@ export default function Index() {
     <View>
       <Text style={{ fontSize: 24 }}>Hello world screen.</Text>
       <Button title="click me" onPress={pressMe} />
+      <Switch onValueChange={toggleSwitch} value={isEnabled}/>
     </View>
   );
 }
